859ec27fa74e287c1c4bb9454193c94d1c6c1ed5
flyofk <ggnekitos@gmail.com>
2026-09-20 20:21:12 +0300

    refactor: restructure group and item menus from the database data

--- keyboards/inline_keyboard.py
+++ keyboards/inline_keyboard.py
@@ -1,4 +1,8 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder, InlineKeyboardMarkup, InlineKeyboardButton
+from sqlalchemy import select
+
+from database.database import SessionLocal
+from database.models import Group, Subject
 
 
 
@@ -20,22 +24,24 @@ def main_menu() -> InlineKeyboardMarkup:
 #     )
 
 
-def item_menu() -> InlineKeyboardMarkup:
-    return InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton(text="Внедрение ИС",callback_data="VIS")],
-            [InlineKeyboardButton(text="Основы Серт. и Станд.",callback_data="OSaS")],
-            [InlineKeyboardButton(text="Основы БД",callback_data="DB")],
-        ]
-    )
+async def item_menu() -> InlineKeyboardMarkup:
+    async with SessionLocal() as session:
+        subjects = (await session.scalars(select(Subject.name))).all()
+        ikb = InlineKeyboardBuilder()
+        for subject in subjects:
+            ikb.add(InlineKeyboardButton(text=subject, callback_data=f"{subject}"))
 
 
-def group_menu() -> InlineKeyboardMarkup:
-    return InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton(text="09c33",callback_data="09c33")],
-            [InlineKeyboardButton(text="09c34",callback_data="09c34")],
-            [InlineKeyboardButton(text="09c35",callback_data="09c35")],
-            [InlineKeyboardButton(text="09c41",callback_data="09c41")]
-        ]
-    )
+        return ikb.as_markup()
+
+
+
+async def group_menu() -> InlineKeyboardMarkup:
+    async with SessionLocal() as session:
+        groups = (await session.scalars(select(Group.name))).all()
+        ikb = InlineKeyboardBuilder()
+        for group in groups:
+            ikb.add(InlineKeyboardButton(text=group, callback_data=f"{group}"))
+
+
+        return ikb.as_markup()
