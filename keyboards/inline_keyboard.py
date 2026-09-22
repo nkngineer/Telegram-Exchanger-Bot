2338ded2a53633808390b0e17498b349adb94465
flyofk <ggnekitos@gmail.com>
2026-09-22 12:22:32 +0300

    refactor: extract get subjects and groups filters fix: add inline keyboard adjustment

--- keyboards/inline_keyboard.py
+++ keyboards/inline_keyboard.py
@@ -1,47 +1,32 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder, InlineKeyboardMarkup, InlineKeyboardButton
-from sqlalchemy import select
-
-from database.database import SessionLocal
-from database.models import Group, Subject
-
+from database.services import get_subjects, get_groups
 
 
 def main_menu() -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton(text="Профиль",callback_data="get_user_profile")],
-            [InlineKeyboardButton(text="Предмет",callback_data="lesson_menu")],
+        inline_keyboard = [
+            [InlineKeyboardButton(text = "Профиль",callback_data = "get_user_profile")],
+            [InlineKeyboardButton(text = "Предмет",callback_data = "lesson_menu")],
         ]
     )
 
-# TODO: добавить функции изменения группы и фамилии с именем
-# def profile_menu() -> InlineKeyboardMarkup:
-#     return InlineKeyboardMarkup(
-#         inline_keyboard=[
-#             [InlineKeyboardButton(text="Изменить фамилию и имя",callback_data="edit_sur_and_last_name")],
-#             [InlineKeyboardButton(text="Изменить группу",callback_data="edit_user_group")],
-#         ]
-#     )
-
 
 async def item_menu() -> InlineKeyboardMarkup:
-    async with SessionLocal() as session:
-        subjects = (await session.scalars(select(Subject.name))).all()
-        ikb = InlineKeyboardBuilder()
-        for subject in subjects:
-            ikb.add(InlineKeyboardButton(text=subject, callback_data=f"{subject}"))
+    subjects = await get_subjects()
+    ikb = InlineKeyboardBuilder()
+    for subject in subjects:
+        ikb.add(InlineKeyboardButton(text = subject.name, callback_data = f"subject_{subject.id}"))
 
-
-        return ikb.as_markup()
+    ikb.adjust(1)
+    return ikb.as_markup()
 
 
 
 async def group_menu() -> InlineKeyboardMarkup:
-    async with SessionLocal() as session:
-        groups = (await session.scalars(select(Group.name))).all()
-        ikb = InlineKeyboardBuilder()
-        for group in groups:
-            ikb.add(InlineKeyboardButton(text=group, callback_data=f"{group}"))
+    groups = await get_groups()
+    ikb = InlineKeyboardBuilder()
+    for group in groups:
+        ikb.add(InlineKeyboardButton(text = group.name, callback_data = f"group_{group.id}"))
 
-
-        return ikb.as_markup()
+    ikb.adjust(1)
+    return ikb.as_markup()
